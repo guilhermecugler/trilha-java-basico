0d23c89398e7effe84e61dc2cea93a1a1ab5110e
Guilhermme Cugler <guilhermecugler@gmail.com>
2024-05-02 14:24:35 -0300

    Controle de Fluxo - Desafio DIO

---
+++ ControleFluxo/src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        // terminal.close();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem =  parametroDois - parametroUm;
+
+        System.out.println("Contagem: "+contagem);
+
+        for(int i=0; i < contagem; i++){
+            System.out.println("Imprimindo o número "+(i+1));
+        }
+        
+    }
+}
